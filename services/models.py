5b6404a79c8880c047e7ee7583e78d6f18dd0a66
marcos Almeida <marcoshenriqueb@gmail.com>
2017-06-29 10:57:49 -0300

    Adds services photos

--- services/models.py
+++ services/models.py
@@ -9,6 +9,7 @@ class Service(models.Model):
     description_home = models.TextField("Descrição home", null=True, blank=True)
     icon = models.ImageField("Ícone", upload_to='servicesicons/%Y/%m/%d/', max_length=255, null=True, blank=True)
     photo = models.ImageField("Foto", upload_to='servicesphotos/%Y/%m/%d/', max_length=255, null=True, blank=True)
+    photo_position = models.CharField("Posição da foto", max_length=15, null=True, blank=True)
     created_at = models.DateTimeField("Criado em", auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField("Atualizado em", auto_now=True, auto_now_add=False)
 
