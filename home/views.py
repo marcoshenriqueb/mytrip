6194a6ee1cc20031f3610af791b6cc54df4311d2
marcos Almeida <marcoshenriqueb@gmail.com>
2017-07-13 16:34:50 -0300

    Adds form

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.views.generic.base import TemplateView
+from django.views import View
+from django.template.response import TemplateResponse
+from django.contrib import messages
+from django.core.mail import send_mail
 from contents.models import Content
 from clients.models import Client
 from services.models import Service, Faq
@@ -18,9 +22,7 @@ class HomePageView(TemplateView):
 
 
 class ServicePageView(TemplateView):
-    template_name = "service.html"
-
-    def get_context_data(self, **kwargs):
+    def get(self, request, **kwargs):
         context = super(ServicePageView, self).get_context_data(**kwargs)
         context['services'] = Service.objects.all()
         context['service'] = Service.objects.filter(uri=kwargs['uri']).first()
@@ -29,7 +31,57 @@ class ServicePageView(TemplateView):
         for c in Content.objects.all():
             content[c.key.lower().replace(" ", "_")] = c.text
         context['content'] = content
-        return context
+        return TemplateResponse(request, 'service.html', context)
+
+    def post(self, request, **kwargs):
+        context = super(ServicePageView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.all()
+        context['service'] = Service.objects.filter(uri=kwargs['uri']).first()
+        context['faq'] = Faq.objects.filter(service=context['service'])
+        content = {}
+        for c in Content.objects.all():
+            content[c.key.lower().replace(" ", "_")] = c.text
+        context['content'] = content
+        form = request.POST
+        try:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.error(form['budget'])
+            send_mail(
+                'Contato Site MyView',
+                'Serviço: %s,\n\nNome: %s,\nEmail: %s,\nTelefone: %s,\n N de Passageiros: %s,\n Data inicio: %s\
+                ,\n Data volta: %s,\n Flexibilidade início: %s,\n Flexibilidade volta: %s\
+                ,\n Últimos destinos: %s,\n Viagens Planejadas: %s,\n Preferências: %s\
+                ,\n Interesses: %s,\n Restriçoes alimentares: %s,\n Acomodação: %s\
+                ,\n Notas gerais: %s,\n Destino: %s,\n Orçamento: %s' % (
+                    kwargs['uri'],
+                    form['name'],
+                    form['email'],
+                    form['phone'],
+                    form['passengers'],
+                    form['startdate'],
+                    form['enddate'],
+                    form['startrange'],
+                    form['endrange'],
+                    form['lastdestinations'] or '',
+                    form['plannedtrips'] or '',
+                    form['preferences'] or '',
+                    ', '.join(form.getlist('insterests')) if 'interests' in form else '',
+                    form['foodrestrictions'] or '',
+                    form['accommodation'] or '',
+                    form['notes'] or '',
+                    form['whereto'] if 'whereto' in form else '',
+                    ', '.join(form.getlist('budget')) if 'budget' in form else '',
+                ),
+                'myviewsolutions123@gmail.com',
+                ['marcoshenriqueb@gmail.com',]
+            )
+            messages.add_message(request, messages.INFO, 'Obrigado pelo contato!')
+        except Exception as e:
+            messages.add_message(request, messages.INFO, e)
+            # messages.add_message(request, messages.INFO, 'Não conseguimos enviar a mensagem, por favor tente novamente.')
+        context['form'] = form
+        return TemplateResponse(request, 'service.html', context)
 
 
 class CorporateView(TemplateView):
