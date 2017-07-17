fc20051bd2e7eab62eb9b37135cd238fc6cd37dc
marcos Almeida <marcoshenriqueb@gmail.com>
2017-07-17 18:31:31 -0300

    Adds second form

--- home/views.py
+++ home/views.py
@@ -44,40 +44,76 @@ class ServicePageView(TemplateView):
         context['content'] = content
         form = request.POST
         try:
-            send_mail(
-                'Contato Site MyView',
-                'Serviço: %s,\n\nNome: %s,\nEmail: %s,\nTelefone: %s,\n N de Passageiros: %s,\n Data inicio: %s\
-                ,\n Data volta: %s,\n Flexibilidade início: %s,\n Flexibilidade volta: %s\
-                ,\n Últimos destinos: %s,\n Viagens Planejadas: %s,\n Preferências: %s\
-                ,\n Interesses: %s,\n Restriçoes alimentares: %s,\n Acomodação: %s\
-                ,\n Notas gerais: %s,\n Destino: %s,\n Orçamento: %s' % (
-                    kwargs['uri'],
-                    form['name'],
-                    form['email'],
-                    form['phone'],
-                    form['passengers'],
-                    form['startdate'],
-                    form['enddate'],
-                    form['startrange'],
-                    form['endrange'],
-                    form['lastdestinations'] or '',
-                    form['plannedtrips'] or '',
-                    form['preferences'] or '',
-                    ', '.join(form.getlist('insterests')) if 'interests' in form else '',
-                    form['foodrestrictions'] or '',
-                    form['accommodation'] or '',
-                    form['notes'] or '',
-                    form['whereto'] if 'whereto' in form else '',
-                    ', '.join(form.getlist('budget')) if 'budget' in form else '',
-                ),
-                'myviewsolutions123@gmail.com',
-                ['marcoshenriqueb@gmail.com',]
-            )
+            if context['service'].form == 1:
+                send_mail(
+                    'Contato Site MyTrip - Surpresa',
+                    'Serviço: %s,\n\nNome: %s,\nEmail: %s,\nTelefone: %s,\n N de Passageiros: %s,\n Data inicio: %s\
+                    ,\n Data volta: %s,\n Flexibilidade início: %s,\n Flexibilidade volta: %s\
+                    ,\n Últimos destinos: %s,\n Viagens Planejadas: %s,\n Preferências: %s\
+                    ,\n Interesses: %s,\n Restriçoes alimentares: %s,\n Acomodação: %s\
+                    ,\n Notas gerais: %s,\n Destino: %s,\n Orçamento: %s' % (
+                        kwargs['uri'],
+                        form['name'],
+                        form['email'],
+                        form['phone'],
+                        form['passengers'],
+                        form['startdate'],
+                        form['enddate'],
+                        form['startrange'],
+                        form['endrange'],
+                        form['lastdestinations'] or '',
+                        form['plannedtrips'] or '',
+                        form['preferences'] or '',
+                        ', '.join(form.getlist('insterests')) if 'interests' in form else '',
+                        form['foodrestrictions'] or '',
+                        form['accommodation'] or '',
+                        form['notes'] or '',
+                        form['whereto'] if 'whereto' in form else '',
+                        ', '.join(form.getlist('budget')) if 'budget' in form else '',
+                    ),
+                    'myviewsolutions123@gmail.com',
+                    ['contato@myviewsolutions.com', 'thiago@myviewsolutions']
+                )
+            if context['service'].form == 2:
+                send_mail(
+                    'Contato Site MyTrip - Do seu jeito',
+                    'Serviço: %s,\n\nNome: %s,\nEmail: %s,\nTelefone: %s,\n N de Passageiros: %s,\
+                    \n Origem: %s,\n Destinos: %s,\n Data inicio: %s\
+                    ,\n Data volta: %s,\n Flexibilidade início: %s,\n Flexibilidade volta: %s\
+                    ,\n Observações: %s,\n Passagem: %s,\n Hotel: %s\
+                    ,\n Tipo de experiência: %s,\n Obs serviço de viagem: %s,\n Obs roteiro personalizado e experiência completa: %s\
+                    ,\n Preferência de hotel ou cia aérea: %s,\n Viaja com animais: %s,\n Necessidades especiais: %s\
+                    ,\n Restrição alimentar: %s' % (
+                        kwargs['uri'],
+                        form['name'],
+                        form['email'],
+                        form['phone'],
+                        form['passengers'],
+                        form['origin'],
+                        form['destination'],
+                        form['startdate'],
+                        form['enddate'],
+                        form['startrange'],
+                        form['endrange'],
+                        form['notes'] or '',
+                        form['ticket'] or '',
+                        form['hotel'] or '',
+                        form['experience'] if 'experience' in form else '',
+                        form['experiencedetail'] or '',
+                        form['experiencedetail2'] or '',
+                        form['preferences'] or '',
+                        form['animals'] or '',
+                        form['special'] or '',
+                        form['food'] or ''
+                    ),
+                    'myviewsolutions123@gmail.com',
+                    ['contato@myviewsolutions.com', 'thiago@myviewsolutions']
+                )
             messages.add_message(request, messages.INFO, 'Obrigado pelo contato, retornaremos em breve!')
         except Exception as e:
             # import logging
             # logger = logging.getLogger(__name__)
-            # logger.error(form['budget'])
+            # logger.error(e)
             messages.add_message(request, messages.INFO, 'Não conseguimos enviar a mensagem, por favor tente novamente.')
             context['form'] = form
         return TemplateResponse(request, 'service.html', context)
